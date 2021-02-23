575456254d9363617af91c95b506612ae7e00749
Max-Laser <78319984+Max-Laser@users.noreply.github.com>
2021-02-23 10:05:42 +0100

    done

--- Uebung5f).py
+++ Uebung5f).py
@@ -38,19 +38,28 @@ u3approx = np.zeros((np.shape(u3)))
 t = np.zeros((np.shape(u1)))
 
 for i in range(0, 100):
-    u1approx[i] = (m1 + m2)*0.01*(q1AblAbl[i]+g)
-    u2approx[i] = m2*0.01*(2*q3Abl[i]*q2Abl[i]*q3[i]+(q3[i]**2)*q2AblAbl[i])
-    u3approx[i] = m2*0.01*(q3AblAbl[i]-q3[i]*(q2Abl[i]**2))
+    u1approx[i] = (m1 + m2)*(q1AblAbl[i]+g)
+    u2approx[i] = m2*(2*q3Abl[i]*q2Abl[i]*q3[i]+(q3[i]**2)*q2AblAbl[i])
+    u3approx[i] = m2*(q3AblAbl[i]-q3[i]*(q2Abl[i]**2))
     if i>0:
         t[i] = t[i-1]+ 0.002
     else:
         t[i] = 0
 
-names = ['Desired_1', 'Desired_2']
-plt.legend(tuple(names))
-plt.xlabel('time[s]', fontsize=15)
-plt.ylabel('u1[N]', fontsize=15)
+#names = ['Desired_1', 'Desired_2']
+#plt.legend(tuple(names))
+#plt.xlabel('time[s]', fontsize=15)
+#plt.ylabel('u1[N]', fontsize=15)
 
-plt.plot(t,u1)
-plt.plot(t,u1approx)
+plt.plot(t,u1,label='u1')
+plt.plot(t,u1approx, label='u1approx')
+plt.legend()
+plt.show()
+plt.plot(t,u2,label='u2')
+plt.plot(t,u2approx, label='u2approx')
+plt.legend()
+plt.show()
+plt.plot(t,u3,label='u3')
+plt.plot(t,u3approx, label='u3approx')
+plt.legend()
 plt.show()
