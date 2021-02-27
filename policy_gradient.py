be46bb795e0e4f4834116fa21dda8a8c3e3413ba
Max-Laser <78319984+Max-Laser@users.noreply.github.com>
2021-02-27 11:10:12 +0100

    done

--- policy_gradient.py
+++ policy_gradient.py
@@ -9,16 +9,16 @@ np.set_printoptions(precision=3, linewidth=100000)
 env = Pend2dBallThrowDMP()
 
 numDim = 10
-numSamples = 5#25
-maxIter = 10#100
-numTrials = 3#10
+numSamples = 25
+maxIter = 100
+numTrials = 10
 
 def mean_confidence_interval(data, confidence=0.95):
     a = 1.0 * np.array(data)
     n = len(a)
     m, se = np.mean(a), scipy.stats.sem(a)
     h = se * scipy.stats.t.ppf((1 + confidence) / 2., n - 1)
-    return m, m - h, m + h
+    return m,h
 
 # YOUR CODE HERE
 theta = np.zeros((numSamples,1))
@@ -32,11 +32,15 @@ mu1=np.zeros((numTrials,1))
 mu2=np.zeros((numTrials,1))
 mu3=np.zeros((numTrials,1))
 
-rew_mean1=np.zeros((numTrials,1))
-rew_mean2=np.zeros((numTrials,1))
-rew_mean3=np.zeros((numTrials,1))
 
-x_achse=np.zeros((numTrials,1))
+rew_mean1=np.zeros((numTrials,))
+rew_mean2=np.zeros((numTrials,))
+rew_mean3=np.zeros((numTrials,))
+
+x_achse=np.zeros((numTrials))
+x_achse2=np.zeros((numTrials))
+
+error=np.zeros((numTrials,))
 
 for k in range(0,numTrials):
     for i in range(0,maxIter):
@@ -61,10 +65,11 @@ for k in range(0,numTrials):
         print(env.getReward(temp))
         """""
         print(i)
-    x_achse[k]=k+1
+    x_achse[k]=k
+    x_achse2[k] = k
     print(k)
     #mu1[k],mu2[k],mu3[k]= mean_confidence_interval(data=rew_mean)
-    rew_mean1[k],rew_mean2[k],rew_mean3[k]= mean_confidence_interval(data=mu)
+    rew_mean1[k],error[k]= mean_confidence_interval(data=mu)
 """""
 plt.plot(x_achse,mu1,label='mean')
 plt.plot(x_achse,mu2)
@@ -72,12 +77,17 @@ plt.plot(x_achse,mu3)
 plt.legend()
 plt.show()
 """""
-#x_achse=np.array([0,1,2])
-#plt.fill_between(x_achse, np.transpose(rew_mean3), np.transpose(rew_mean2), color='c', label='95% confidence')
-plt.plot(np.transpose(x_achse),np.transpose(rew_mean1),label='average return')
-plt.plot(np.transpose(x_achse),np.transpose(rew_mean2))
-plt.plot(np.transpose(x_achse),np.transpose(rew_mean3))
+'x_achse=np.array([0,1,2])'
+
+wtf=np.array([-2.7,-0.61488,-0.139])
+array= np.zeros((3,))
+array[1]=5
+plt.plot(x_achse,np.transpose(rew_mean1),'ob',label='average return')
+plt.errorbar(x_achse, np.transpose(rew_mean1) ,yerr = error,fmt ='o')
+#plt.fill_between(np.array([0,1,2]), rew_mean3, rew_mean2,"ob", color='c', label='95% confidence')
+
 plt.legend()
+#plt.figure()
 plt.show()
 
 
