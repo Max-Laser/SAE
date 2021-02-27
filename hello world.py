59d91f7527765af141b7784b8e22652f3356d474
Max-Laser <78319984+Max-Laser@users.noreply.github.com>
2021-02-27 16:45:47 +0100

    done

--- hello world.py
+++ hello world.py
@@ -1,1 +1,7 @@
-print ("hello world")
+import numpy as np
+
+x= np.array([1,3])
+y= np.array([2,5])
+
+z= x @ y
+print(z)
--- policy_gradient.py
+++ policy_gradient.py
@@ -11,7 +11,7 @@ env = Pend2dBallThrowDMP()
 numDim = 10
 numSamples = 25
 maxIter = 100
-numTrials = 10
+numTrials =10
 
 def mean_confidence_interval(data, confidence=0.95):
     a = 1.0 * np.array(data)
@@ -21,37 +21,24 @@ def mean_confidence_interval(data, confidence=0.95):
     return m,h
 
 # YOUR CODE HERE
-theta = np.zeros((numSamples,1))
-grad_sample = np.zeros((numSamples,1))
-mu=np.zeros((maxIter+1,1))
-sigma=10
-rew = np.zeros((numSamples,1))
-rew_mean=np.zeros((maxIter+1,1))
 
-mu1=np.zeros((numTrials,1))
-mu2=np.zeros((numTrials,1))
-mu3=np.zeros((numTrials,1))
+grad_sample = np.zeros((numSamples,))
+mu=np.zeros((numDim,))
+Sigma=np.diag(np.array([10, 10, 10, 10, 10, 10, 10, 10, 10, 10]))#TODO evtl 100
+rew = np.zeros((numSamples,))
+rew_step=np.zeros((maxIter,))
+rew_mean=np.zeros((numTrials,))
 
-
-rew_mean1=np.zeros((numTrials,))
-rew_mean2=np.zeros((numTrials,))
-rew_mean3=np.zeros((numTrials,))
-
+#To plot
 x_achse=np.zeros((numTrials))
-x_achse2=np.zeros((numTrials))
-
 error=np.zeros((numTrials,))
 
 for k in range(0,numTrials):
     for i in range(0,maxIter):
         for t in range(0,numSamples):
-
-            theta= np.random.normal(mu[i],10)
-            temp=np.zeros((5,1))
-            for h in range(0,5):
-                temp[h]=theta
-            rew[t]= env.getReward(np.transpose(np.transpose(temp)))
-            grad_sample[t] = 1 / maxIter * 1 / (100) ** 2 * (theta - mu[i]) * rew[t]
+            theta= np.random.multivariate_normal(mu, Sigma)
+            rew[t]= env.getReward(theta)
+            grad_sample[t] = (1 / maxIter) * np.linalg.inv(Sigma) * (theta - mu[i]) * (rew[t] - 0.01)
         #theta_mean = np.mean(theta)
         #rew_mean= np.mean(rew)
         grad= np.mean(grad_sample)
@@ -65,29 +52,14 @@ for k in range(0,numTrials):
         print(env.getReward(temp))
         """""
         print(i)
-    x_achse[k]=k
-    x_achse2[k] = k
-    print(k)
-    #mu1[k],mu2[k],mu3[k]= mean_confidence_interval(data=rew_mean)
-    rew_mean1[k],error[k]= mean_confidence_interval(data=mu)
-"""""
-plt.plot(x_achse,mu1,label='mean')
-plt.plot(x_achse,mu2)
-plt.plot(x_achse,mu3)
-plt.legend()
-plt.show()
-"""""
-'x_achse=np.array([0,1,2])'
 
-wtf=np.array([-2.7,-0.61488,-0.139])
-array= np.zeros((3,))
-array[1]=5
+    x_achse[k]=k
+    rew_mean[k],error[k]= mean_confidence_interval(data=mu)
+
+
 plt.plot(x_achse,np.transpose(rew_mean1),'ob',label='average return')
 plt.errorbar(x_achse, np.transpose(rew_mean1) ,yerr = error,fmt ='o')
-#plt.fill_between(np.array([0,1,2]), rew_mean3, rew_mean2,"ob", color='c', label='95% confidence')
-
 plt.legend()
-#plt.figure()
 plt.show()
 
 
